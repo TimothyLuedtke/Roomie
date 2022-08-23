b2e33168c63ba0cf6fb9c270b8f4bf11b1806ff8
Timothy Luedtke <timothyluedtke@gmail.com>
2022-08-23 14:09:07 -0500

    kk

--- components/Tasks/TaskRow.js
+++ components/Tasks/TaskRow.js
@@ -1,6 +1,24 @@
+import { Button } from "@chakra-ui/react";
 
 export const TaskRow = ({ taskItem }) => {
     
+// create a function the handles the expansion of a taskItem to show the task description
+    const handleExpand = () => {
+        
+        return (
+            <>
+                <tr>
+                <td>{taskItem.completed}</td>
+                    <td>{taskItem.due}</td>
+                <Button size="sm" variant="outline">Edit</Button>
+                </tr>
+                <td>{taskItem.description}</td>
+                <td>{taskItem.taskList}</td>
+            </>
+        )
+    }         
+
+
     return (
         <tr>
         <td>{taskItem.taskName}</td>
