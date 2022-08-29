3a62cf4be22e9469f5721282a16acad42ac1975b
Timothy Luedtke <timothyluedtke@gmail.com>
2022-08-29 11:14:22 -0500

    delete works, so does add task and reset form

--- components/Tasks/TaskList.js
+++ components/Tasks/TaskList.js
@@ -33,14 +33,21 @@ import { nanoid } from "nanoid";
 
 
 export const TaskList = () => {
-    
-    const [taskList, setTaskList] = useState(TaskData);
-    ;
 
-    const handleDelete = (id) => {
-        const newTaskList = taskList.filter((task) => task.id !== id);
-        setTaskList(newTaskList);
-    };
+    const [taskList, setTaskList] = useState(TaskData);
+    const [taskName, setTaskName] = useState("");
+    const [description, setDescription] = useState("");
+    const [due_date, setDue_date] = useState("");
+    const [assigned_to, setAssigned_to] = useState("");
+    const [listName, setListName] = useState("");
+
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+
+    const toast = useToast();
+
 
     const handleComplete = (id) => {
         const newTaskList = taskList.map((task) => {
@@ -53,141 +60,155 @@ export const TaskList = () => {
         setTaskList(newTaskList);
     };
 
-
-
-
-    const AddTask = () => {
-
-        const { isOpen, onOpen, onClose } = useDisclosure();
-        const [taskName, setTaskName] = useState("");
-        const [description, setDescription] = useState("");
-        const [due_date, setDue_date] = useState("");
-        const [assigned_to, setAssigned_to] = useState("");
-        const [listName, setListName] = useState("");
-        const toast = useToast();
-    
-    
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            const newTask = {
-                id: nanoid(),
-                taskName,
-                description,
-                due_date,
-                assigned_to,
-                listName,
-                completed: false,
-            };
-    
-            const newTaskList = [{ ...taskList }, newTask];
-            setTaskList(newTaskList);
-            toast({
-                title: "Task Added",
-                description: "We've added your task to the list.",
-                status: "success",
-                duration: 9000,
-                isClosable: true,
-            });
-            onClose();
-        };
-    
-        return (
-            <>
-                <Button onClick={onOpen}>Add Task</Button>
-                <Modal isOpen={isOpen} onClose={onClose}>
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>Add Task</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                            <Stack spacing={3}>
-                                <Input
-                                    placeholder="Task Name"
-                                    onChange={(e) => setTaskName(e.target.value)}
-                                />
-                                <Input
-                                    placeholder="Description"
-                                    onChange={(e) => setDescription(e.target.value)}
-                                />
-                                <Input
-                                    placeholder="Due Date"
-                                    onChange={(e) => setDue_date(e.target.value)}
-                                />
-                                <Input
-                                    placeholder="Assigned To"
-                                    onChange={(e) => setAssigned_to(e.target.value)}
-                                />
-                                <Input
-                                    placeholder="List Name"
-                                    onChange={(e) => setListName(e.target.value)}
-                                />
-                            </Stack>
-                        </ModalBody>
-    
-                        <ModalFooter>
-                            <Button colorScheme="blue" mr={3} onClick={ handleSubmit }>
-                                Add Task
-                            </Button>
-                            <Button onClick={onClose}>Cancel</Button>
-                        </ModalFooter>
-                    </ModalContent>
-                </Modal>
-            </>
-        );
+    const handleAdd = (newtask) => {
+        const newTaskList = [...taskList, newtask];
+        setTaskList(newTaskList);
     };
 
 
 
 
+    const handleDelete = (id) => {
+        const newTaskList = taskList.filter((task) => task.id !== id);
+        setTaskList(newTaskList);
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTask = {
+            id: nanoid(),
+            taskName,
+            description,
+            due_date,
+            assigned_to,
+            listName,
+            completed: false,
+
+        };
+        handleAdd(newTask);
+        onClose();
+        toast({
+            title: "Task added.",
+            description: "We've added your task to the list.",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+        });
+
+        resetForm();
+    };
+
+    const resetForm = () => {
+        setTaskName("");
+        setDescription("");
+        setDue_date("");
+        setAssigned_to("");
+        setListName("");
+    };
+
+
 
     return (
         <>
-        <Container>
-            <Flex direction="column">
-                <Accordion allowMultiple>
+            <Container>
+                <Flex>
 
-            {taskList.map((task) => (
-                <AccordionItem key={task.id}>
-                    <h2>
-                        <AccordionButton>
-                            <Checkbox
-                                padding={2}
-                                isChecked={task.completed}
-                                onChange={() => handleComplete(task.id)}
-                                id={task.id}
+                    <Text fontSize="4xl" fontWeight="bold">
+                        Tasks
+                    </Text>
+                    <Spacer />
+                    <Box>
+                        <Button colorScheme="teal" onClick={onOpen}>
+                            Add Task
+                        </Button>
+                    </Box>
+                </Flex>
+                <Flex direction="column">
+                    <Accordion allowMultiple>
+
+                        {taskList.map((task) => (
+                            <AccordionItem key={task.id}>
+                                <h2>
+                                    <AccordionButton>
+                                        <Checkbox
+                                            padding={2}
+                                            isChecked={task.completed}
+                                            onChange={() => handleComplete(task.id)}
+                                            id={task.id}
 
 
-                            />
-                            <Box flex="1" textAlign="left" marginLeft={10}>
-                                <Text>{task.taskName}</Text>
-                            </Box>
-                            <AccordionIcon />
-                        </AccordionButton>
-                    </h2>
-                    <AccordionPanel pb={4}>
-                        <Flex direction="column">
-                            <Text>Description: {task.description}</Text>
-                            <Text>Due: {task.due_date}</Text>
-                            <Text>Assigned to: {task.assigned_to}</Text>
-                            <Text>List: {task.listName}</Text>
-                        </Flex>
-                        <Flex direction="row" justifyContent="right">
-                            <Button
-                                variant="outline"
-                                onClick={() => handleDelete(task.id)}
-                            >
-                                Delete
-                            </Button>
+                                        />
+                                        <Box flex="1" textAlign="left" marginLeft={10}>
+                                            <Text>{task.taskName}</Text>
+                                        </Box>
+                                        <AccordionIcon />
+                                    </AccordionButton>
+                                </h2>
+                                <AccordionPanel pb={4}>
+                                    <Flex direction="column">
+                                        <Text>Description: {task.description}</Text>
+                                        <Text>Due: {task.due_date}</Text>
+                                        <Text>Assigned to: {task.assigned_to}</Text>
+                                        <Text>List: {task.listName}</Text>
+                                    </Flex>
+                                    <Flex direction="row" justifyContent="right">
+                                        <Button
+                                            variant="outline"
+                                            onClick={() => handleDelete(task.id)}
+                                        >
+                                            Delete
+                                        </Button>
 
-                        </Flex>
-                    </AccordionPanel>
-                </AccordionItem>
+                                    </Flex>
+                                </AccordionPanel>
+                            </AccordionItem>
 
-            ))}
-            </Accordion>
-            <AddTask />
-            </Flex>
-        </Container>
+                        ))}
+                    </Accordion>
+                    <>
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                            <ModalOverlay />
+                            <ModalContent>
+                                <ModalHeader>Add Task</ModalHeader>
+                                <ModalCloseButton />
+                                <ModalBody>
+                                    <Stack spacing={3}>
+                                        <Input
+                                            placeholder="Task Name"
+                                            onChange={(e) => setTaskName(e.target.value)}
+                                        />
+                                        <Input
+                                            placeholder="Description"
+                                            onChange={(e) => setDescription(e.target.value)}
+                                        />
+                                        <Input
+                                            placeholder="Due Date"
+                                            onChange={(e) => setDue_date(e.target.value)}
+                                        />
+                                        <Input
+                                            placeholder="Assigned To"
+                                            onChange={(e) => setAssigned_to(e.target.value)}
+                                        />
+                                        <Input
+                                            placeholder="List Name"
+                                            onChange={(e) => setListName(e.target.value)}
+                                        />
+                                    </Stack>
+                                </ModalBody>
+
+                                <ModalFooter>
+                                    <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
+                                        Add Task
+                                    </Button>
+                                    <Button onClick={onClose}>Cancel</Button>
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </>
+
+                </Flex>
+            </Container>
         </>
     );
 };
