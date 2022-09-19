96c9cf4c462ed8d69225a9623e7f099b4d43bccd
Timothy Luedtke <timothyluedtke@gmail.com>
2022-09-18 22:48:56 -0500

    adding form submit

--- pages/SignupPage.js
+++ pages/SignupPage.js
@@ -30,6 +30,17 @@ export default function Signup() {
             <Head />
             <TopNavbar /> 
                   
+            <form onSubmit={AddUser}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+                <button type="submit">Sign Up</button>
+            </form>
+
+            
             <BottomNavbar />
         </>
     )
